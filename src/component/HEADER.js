8abe5eb25c22a5c671047b83d0910a6490135fca
highcastle4242 <wnstjd4242@naver.com>
2020-09-13 00:55:51 +0900

    master commit

---
+++ src/component/HEADER.js
@@ -0,0 +1,100 @@
+import React,{ Component } from 'react';
+import './VMC.css';
+
+class HEADER extends Component{
+  constructor(props){
+      super(props);
+      this.state={
+          menu:true,
+          matches: window.matchMedia("(max-width: 1024px)").matches,
+          lists:[
+            {key:"1", title:"HOME", id:"HOME"},
+            {key:"2", title:"ABOUT", id:"ABOUT"},
+            {key:"3", title:"ARTIST", id:"ARTIST"},
+            {key:"4", title:"STORE", id:"https://smartstore.naver.com/vmc_store"},
+            {key:"5", title:"CONTACT", id:"CONTACT"}
+          ],
+          icons:[
+              {key:'facebook',href:'https://www.facebook.com/vismajorcrew',src:"facebook"},
+              {key:'instagram',href:'https://www.instagram.com/vismajor_company',src:'instagram'},
+              {key:'twitter',href:'https://twitter.com/vismajorcompany',src:'twitter'},
+              {key:'youtube',href:'https://www.youtube.com/user/vismajorcompany',src:'youtube'},
+              {key:'soundcloud',href:'https://soundcloud.com/vmcofficial',src:'soundcloud'}
+          ]
+      };
+      this.onChangeMenu = this.onChangeMenu.bind(this);
+  }
+  componentDidMount() {
+    const handler = e => this.setState({matches: e.matches});
+    window.matchMedia("(max-width: 1024px)").addListener(handler);
+  }
+  onChangeMenu(){
+    this.setState(state =>({
+      menu:!state.menu
+    }));
+  }
+  render(){
+    var listTag = [];
+    var iconTag = [];
+    for(var i = 0, li; i<this.state.lists.length; i++){
+      li = this.state.lists[i];
+      if(i===3){
+        listTag.push(<li key={li.key}><a href={'https://smartstore.naver.com/vmc_store'}
+        target='_blank' rel="noopener noreferrer">{li.title}</a></li>);
+      }else{
+        listTag.push(<li key={li.key}><a href="#"
+        onClick={function(id,e){
+          e.preventDefault();
+          this.props.onChangePage(id);
+        }.bind(this,li.id)}>{li.title}</a></li>);
+      }
+    }
+    for(var j = 0,icon; j<this.state.icons.length;j++){
+        icon = this.state.icons[j];
+        iconTag.push(<li key={icon.key}><a href={icon.href}><img src={require('./images/'+icon.src+'.png')} alt="icon"/></a></li>)
+    }
+    var wideMenu;
+    if (this.state.menu===true){
+      wideMenu={display:'none'};
+    }else if(this.state.menu===false){
+      wideMenu={display:'block'};
+    }
+    return(
+    <header>
+        <nav className='header-bar'>
+            <h1 className='main-logo'>
+                <div id='logo' onClick={function(id,e){
+                  e.preventDefault();
+                  this.props.onChangePage(id);
+                }.bind(this,this.state.lists[0].id)}><img src={require('./images/vmc_logo.png')} alt="logo"/></div>
+            </h1>
+            <div id='wide-menu'>
+                <ul id='nav-text'>
+                    {listTag}
+                </ul>
+                <ul id='nav-logo'>
+                    {iconTag}
+                </ul>
+            </div>
+            {this.state.matches && (
+            <div id='wide-menu' style={wideMenu}>
+              <ul id='nav-text'>
+                  {listTag}
+              </ul>
+              <ul id='nav-logo'>
+                  {iconTag}
+              </ul>
+            </div>)}
+            {this.state.matches && (
+            <div className='m-menu' style={{display:'block !important'}}>
+                <button id='m-menu-btn' onClick={this.onChangeMenu}>
+                  {this.state.menu ? 'M' : 'X'}
+                </button>
+            </div>)}
+        </nav>
+    </header>
+    );
+  }
+}
+
+export default HEADER;
---
+++ src/component/HOME.js
@@ -0,0 +1,36 @@
+import React,{ Component } from 'react';
+import './VMC.css';
+
+class HOME extends Component{
+  render(){
+    return(
+      <div className='content'>
+        <section className='youtube'>
+          <iframe title="frame" width="1126" height="634" src="https://www.youtube.com/embed/jOgLkdZJOJQ" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </section>
+        <article className='instagram'>
+        <p className="insta-txt">
+            <span className="txt1">INSTAGRAM</span>
+            <span className="txt2">VMC OFFICIAL INSTAGRAM</span>
+            <span className="txt3"># VISMAJOR CREW # Since 2011 # VISMAJOR COMPANY # Since 2014</span>
+        </p>
+        <ul className='insta-list'>
+            <li><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img src="https://pbs.twimg.com/media/EBbTWAuVAAAfWsQ?format=jpg&name=900x900" alt='instagram'/></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img src="https://pbs.twimg.com/media/EBbTWAvVAAA-4Fb?format=jpg&name=900x900" alt='instagram'/></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img src="https://pbs.twimg.com/media/EBbTWAvUIAADTiB?format=jpg&name=900x900" alt='instagram'/></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img src="https://pbs.twimg.com/media/EBbTWAwU4AAugjD?format=jpg&name=900x900" alt='instagram'/></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img src="https://pbs.twimg.com/media/EBbTWAuVAAAfWsQ?format=jpg&name=900x900" alt='instagram'/></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img src="https://pbs.twimg.com/media/EBbTWAvVAAA-4Fb?format=jpg&name=900x900" alt='instagram'/></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img src="https://pbs.twimg.com/media/EBbTWAvUIAADTiB?format=jpg&name=900x900" alt='instagram'/></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><img src="https://pbs.twimg.com/media/EBbTWAwU4AAugjD?format=jpg&name=900x900" alt='instagram'/></a></li>
+        </ul>
+        <div className='follow'>
+            <a href='https://www.instagram.com/vismajor_company/?hl=ko' target="_blank" rel="noopener noreferrer"><button id='btn-follow' ><span>Follow</span></button></a>
+        </div>
+    </article>
+      </div>
+    );
+  }
+}
+
+export default HOME;
